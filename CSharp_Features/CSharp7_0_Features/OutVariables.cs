5f0b3a0665633be9dc7e0614a539bb13495473f7
saikumar.koona <saikumar.koona@ggktech.com>
2018-09-30 14:23:54 +0530

    OutVariables

---
+++ CSharp_Features/CSharp7_0_Features/OutVariables.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp7_0_Features
+{
+    public class OutVariablesE1Old
+    {
+        public void GetEmployeeDetails()
+        {
+            double salary;
+            GetSalary(out salary);
+        }
+
+        private void GetSalary(out double salary)
+        {
+            salary = 10000;
+        }
+    }
+
+    public class OutVariablesE1
+    {
+        public void GetEmployeeDetails()
+        {
+            GetSalary(out var salary);
+            
+            if(int.TryParse(GetAge(), out var age))
+            {
+                //Do something here
+            }
+        }
+
+        private string GetAge()
+        {
+            return "25";
+        }
+        private void GetSalary(out double salary)
+        {
+            salary = 10000;
+        }
+    }
+
+}
