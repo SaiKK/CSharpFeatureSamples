7232576ec0489e806c541697f3e14408d3e46895
saikumar.koona <saikumar.koona@ggktech.com>
2018-09-30 23:50:02 +0530

    Switch Statement

---
+++ CSharp_Features/CSharp7_0_Features/SwitchStatements.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp7_0_Features.Switch
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+    }
+    public class Dog : Animal
+    {
+
+    }
+    public class Cat : Animal
+    {
+
+    }
+
+    public class SwitchStatements
+    {
+        public void SwitchDemo()
+        {
+            object obj = new Dog() { Name = "Dog" };
+
+            switch(obj)
+            {
+                case Dog dog: Console.WriteLine(dog.Name); break;
+                case Cat cat: Console.WriteLine(cat.Name); break;
+                case null: Console.WriteLine("Not Assign"); break;
+                default: Console.WriteLine("No match found"); break;
+            }
+        }
+
+        public void SwitchDemoWithCondition()
+        {
+            object obj = new Dog() { Name = "Dog" };
+
+            switch (obj)
+            {
+                case Dog dog when dog.Name == "Dog": Console.WriteLine(dog.Name); break;
+                case Dog dog when dog.Name == "Doggy": Console.WriteLine(dog.Name); break;
+                case Cat cat: Console.WriteLine(cat.Name); break;
+                case null: Console.WriteLine("Not Assign"); break;
+                default: Console.WriteLine("No match found"); break;
+            }
+        }
+    }
+}
