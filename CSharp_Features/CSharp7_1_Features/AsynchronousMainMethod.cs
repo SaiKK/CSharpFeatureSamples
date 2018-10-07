e84410a007aedc4637ea02d937b99aa99e4000a5
Sai Kumar Koona <saikk.techie@gmail.com>
2018-10-07 17:23:45 +0530

    Async Main Method

---
+++ CSharp_Features/CSharp7_1_Features/AsynchronousMainMethod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp7_1_Features
+{
+    public class AsynchronousMainMethod
+    {
+        public static void Main(string[] args)
+        {
+            MyMainAsync(args).GetAwaiter().GetResult();
+        }
+
+        public static async Task MyMainAsync(string[] args)
+        {
+            //Do Some Logic here.
+        }
+    }
+
+    public class AsynchronousMainMethod_7_1
+    {
+        public static async Task Main(string[] args)
+        {
+            //Do Some Logic here.
+        }
+
+    }
+}
